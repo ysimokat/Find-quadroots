787467b7657f48d1c296a45fcff1527b0c3f95c5
Simokat <yanhong7369@gmail.com>
2016-07-16 00:17:48 -0500

    add functions listsd,listsf,listsg for 3 cases when a=0

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ import cmath
     -convert each string of three characters to a list of character
     -convert a list of three strings to floats a,b,c,print a,b,c out
     -calculate descriminant d = b^2 - 4ac
-    -calculate quadratic x1 = (-b + sqrt(d))/(2a)
-    -calculate quadratic x2 = (-b - sqrt(d))/(2a)
+    -solve polynomial function
     -write output to 'quadroots.txt'
 """
 
@@ -27,8 +26,23 @@ def get_abc(line):
     return a,b,c
 
 def descriminant(a,b,c):
+    """ calculate descriminant """
     d = b ** 2 - 4 * a * c
     return d
+
+def rootsd(a,b,c):
+    """ a = 0, all complex numbers are roots """
+    return "All complex numbers are roots" 
+
+def rootsf(a,b,c):
+    """ when a = 0, no real/complex roots """
+    if b == 0:
+        return False
+
+def rootsg(a,b,c):
+    """ to find one root """
+    root2 = - c / b
+    return root2
 
 def rootsi(a,b):
     """ b^2-4ac = 0, perfect roots """
@@ -59,23 +73,42 @@ def main():
         # print line  # for debugging
         a,b,c = get_abc(line) # extract a,b,c
         print "(a,b,c): ", a,b,c 
-        print "-------------------------------------"
         if a == 0:     #specail cases where a = 0
-            print "-----------do nothing where a = 0-------"
+            if  b == 0:
+                if c == 0:
+                    case1 = rootsd(a,b,c)
+                    print case1
+                    print
+                elif c != 0:
+                    case2 = rootsf(a,b,c)   
+                    if case2 == False:
+                        print "No real or complex roots"
+                        print
+            elif b != 0:
+                if c == 0:
+                     x = 0
+                     print x
+                     print
+                elif c != 0:
+                    case3 = rootsg(a,b,c)
+                    print case3
+                    print
+
         elif a != 0:  # use the quadratic formula
             d = descriminant(a,b,c)  # discriminant
-            print d
-            print "---------------------"
+            #print d # for debugging
             if d == 0:  # case 4: perfect square
                 case4 = rootsi(a,b)
                 print case4
+                print
             elif d > 0:  # case 5: two real roots
                 case5 = rootsj(a,b,d)
                 print case5
+                print
             elif d < 0:  # case 6: two complex roots
                 i = cmath.sqrt(-1)
                 case6 = rootsk(a,b,d,i)
-                print case6
+                print case6   
     infile.close()
 if __name__ == "__main__":
     main()
